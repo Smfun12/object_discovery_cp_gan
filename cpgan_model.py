ab0dba6ec14f78282ba39eebf26d68dbec45d139
sashaS1imple <sasharesh12@gmail.com>
2023-04-21 13:45:21 +0200

    cp-gan

--- cpgan_model.py
+++ cpgan_model.py
@@ -6,8 +6,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from cpgan_data import *
-from cpgan_tools import *
+
 
 
 def create_gaussian_filter(blur_sigma):
